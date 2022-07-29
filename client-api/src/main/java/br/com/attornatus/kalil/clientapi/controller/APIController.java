8e12831ca5f870cd0a14bb3651a145b9cc93e9d3
kak4k0ur1 <kjfakhouri@gmail.com>
2022-07-29 16:51:08 -0300

    API fixed

--- client-api/src/main/java/br/com/attornatus/kalil/clientapi/controller/APIController.java
+++ client-api/src/main/java/br/com/attornatus/kalil/clientapi/controller/APIController.java
@@ -3,10 +3,12 @@ package br.com.attornatus.kalil.clientapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.attornatus.kalil.clientapi.database.PessoaData;
@@ -25,6 +27,7 @@ public class APIController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
 	public Pessoa post(@RequestBody Pessoa pessoa) {
 		return pessoaData.save(pessoa);
 	}
--- client-api/src/main/java/br/com/attornatus/kalil/clientapi/template/Pessoa.java
+++ client-api/src/main/java/br/com/attornatus/kalil/clientapi/template/Pessoa.java
@@ -2,31 +2,88 @@ package br.com.attornatus.kalil.clientapi.template;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import lombok.Data;
 
-@Data
 @Entity
 public class Pessoa {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	
 	@Column(nullable = false)
 	private String nome;
+
+	@Column(nullable = false)
+	private LocalDate aniversario;
 	
 	@Column(nullable = false)
-	private LocalDate dataNascimento;
+	private ArrayList<String> endereco;
 	
-	@Column(nullable = false)
-	private String endereco;
-	
-	@Column(nullable = false)
+	@Column 
 	private byte enderecoPrincipal;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getAniversario() {
+		return aniversario;
+	}
+
+	public void setAniversario(LocalDate aniversario) {
+		this.aniversario = aniversario;
+	}
+
+	public ArrayList<String> getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(ArrayList<String> endereco) {
+		this.endereco = endereco;
+	}
+
+	public byte getEnderecoPrincipal() {
+		return enderecoPrincipal;
+	}
+
+	public void setEnderecoPrincipal(byte enderecoPrincipal) {
+		this.enderecoPrincipal = enderecoPrincipal;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		return Objects.equals(id, other.id);
+	}
 	
 }
