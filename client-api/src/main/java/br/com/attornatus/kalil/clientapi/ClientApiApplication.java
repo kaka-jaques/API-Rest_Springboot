c01648daef32486d113a99f0d32bfffac1f38f1e
kak4k0ur1 <kjfakhouri@gmail.com>
2022-08-02 11:30:38 -0300

    UPDATE!
    Own Mark added on API, new Properties for H2 DB

--- client-api/src/main/java/br/com/attornatus/kalil/clientapi/ClientApiApplication.java
+++ client-api/src/main/java/br/com/attornatus/kalil/clientapi/ClientApiApplication.java
@@ -1,13 +1,32 @@
 package br.com.attornatus.kalil.clientapi;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClientApiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		String content;
+		
+		URL url = new URL("https://kaka-jaques.github.io/content/mark.html");
+		
+		BufferedReader page = new BufferedReader(new InputStreamReader(url.openStream()));
+		
+		while((content = page.readLine()) != null) {
+			System.out.println(content);
+		}
+		
 		SpringApplication.run(ClientApiApplication.class, args);
+		
 	}
 
 }
